9e0c71e03596a91c516136fc4c25d59f1ecad8e5
Tobias Krumholz <krumholt@gmail.com>
2023-02-04 01:31:07 +0100

    first try add rendering pixels

--- src/main.c
+++ src/main.c
@@ -14,11 +14,8 @@ void __stdcall FreeConsole(void);
 //----------------------------------------------------------------------------------
 const int screenWidth = 960;
 const int screenHeight = 540;
+Image screen = {0};
 
-//----------------------------------------------------------------------------------
-// Local Functions Declaration
-//----------------------------------------------------------------------------------
-static void UpdateDrawFrame(void);          // Update and draw one frame
 
 //----------------------------------------------------------------------------------
 // Main entry point
@@ -46,70 +43,35 @@ int main()
 #else
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
+    //
+    screen = GenImageColor(screenWidth, screenHeight, DARKBLUE);
+    Texture2D screen_texture = LoadTextureFromImage(screen);
 
+    Color brush[10*10] = {0};
+    for (int index = 0; index < 10*10; ++index)
+    {
+        brush[index] = BLACK;
+    }
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        UpdateDrawFrame();
+        BeginDrawing();
+        //ClearBackground(RAYWHITE);
+        int x = GetMouseX();
+        int y = GetMouseY();
+        Rectangle target = {x, y, 10.0f, 10.0f};
+        UpdateTextureRec(screen_texture, target, brush);
+        DrawTexture(screen_texture, 0, 0, WHITE);
+        EndDrawing();
     }
 #endif
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
     CloseWindow();                  // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-    getch();
+                                    //--------------------------------------------------------------------------------------
     FreeConsole();
 
     return 0;
 }
 
-float rotation = 0.0f;
-// Update and draw game frame
-static void UpdateDrawFrame(void)
-{
-
-    // Update
-    //----------------------------------------------------------------------------------
-    rotation += 0.2f;
-    //----------------------------------------------------------------------------------
-
-    // Draw
-    //----------------------------------------------------------------------------------
-    BeginDrawing();
-
-    ClearBackground(RAYWHITE);
-
-    DrawText("some basic shapes available on raylib", 20, 20, 20, DARKGRAY);
-
-    // Circle shapes and lines
-    DrawCircle(screenWidth/5, 120, 35, DARKBLUE);
-    DrawCircleGradient(screenWidth/5, 220, 60, GREEN, SKYBLUE);
-    DrawCircleLines(screenWidth/5, 340, 80, DARKBLUE);
-
-    // Rectangle shapes and lines
-    DrawRectangle(screenWidth/4*2 - 60, 100, 120, 60, RED);
-    DrawRectangleGradientH(screenWidth/4*2 - 90, 170, 180, 130, MAROON, GOLD);
-    DrawRectangleLines(screenWidth/4*2 - 40, 320, 80, 60, ORANGE);  // NOTE: Uses QUADS internally, not lines
-
-    // Triangle shapes and lines
-    DrawTriangle((Vector2){ screenWidth/4.0f *3.0f, 80.0f },
-                 (Vector2){ screenWidth/4.0f *3.0f - 60.0f, 150.0f },
-                 (Vector2){ screenWidth/4.0f *3.0f + 60.0f, 150.0f }, VIOLET);
-
-    DrawTriangleLines((Vector2){ screenWidth/4.0f*3.0f, 160.0f },
-                      (Vector2){ screenWidth/4.0f*3.0f - 20.0f, 230.0f },
-                      (Vector2){ screenWidth/4.0f*3.0f + 20.0f, 230.0f }, DARKBLUE);
-
-    // Polygon shapes and lines
-    DrawPoly((Vector2){ screenWidth/4.0f*3, 330 }, 6, 80, rotation, BROWN);
-    DrawPolyLines((Vector2){ screenWidth/4.0f*3, 330 }, 6, 90, rotation, BROWN);
-    DrawPolyLinesEx((Vector2){ screenWidth/4.0f*3, 330 }, 6, 85, rotation, 6, BEIGE);
-
-    // NOTE: We draw all LINES based shapes together to optimize internal drawing,
-    // this way, all LINES are rendered in a single draw pass
-    DrawLine(18, 42, screenWidth - 18, 42, BLACK);
-
-    EndDrawing();
-    //----------------------------------------------------------------------------------
-}
