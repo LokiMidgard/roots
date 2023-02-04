d56335b68686e4d91f925221c7edc0440d24bafd
Patrick Kranz <patrick-kranz@live.de>
2023-02-04 12:06:04 +0100

    add rotation

--- src/main.c
+++ src/main.c
@@ -20,30 +20,6 @@
 
 #define NUM_SEEDS (30)
 
-typedef struct Sprite
-{
-    Vector2 position;
-    int speed;
-    int counter;
-    Texture2D image;
-    int number_of_frames;
-} Sprite;
-
-void draw_sprite(Sprite *s)
-{
-    // calculate current frame
-    int current_frame = s->counter / s->speed;
-    int frame_width = s->image.width / s->number_of_frames;
-    int frame_height = s->image.height;
-    Rectangle frame = {current_frame * frame_width, 0, frame_width, frame_height};
-    Rectangle dstRect = {s->position.x * GetScreenWidth() / WIDTH, s->position.y * GetScreenHeight() / HEIGHT, frame_width * GetScreenWidth() / WIDTH, frame_height * GetScreenHeight() / HEIGHT};
-    Vector2 origin = {0, 0};
-    DrawTexturePro(s->image, frame, dstRect, origin, 0.0f, WHITE);
-}
-void update_sprite(Sprite *s)
-{
-    s->counter = (s->counter + 1) % (s->number_of_frames * s->speed);
-}
 
 void scroll_world(Color *world)
 {
@@ -97,17 +73,6 @@ int main()
     Texture2D screen_texture = LoadTextureFromImage(world_image);
     Color *world = LoadImageColors(world_image);
 
-    /***************************************************************************
-     * Create character
-     ****************************************************************************/
-    Sprite mole;
-    mole.counter = 0;
-    mole.image = LoadTexture("resources/mole.png");
-    mole.number_of_frames = 8;
-    mole.position.x = 30;
-    mole.position.y = 30;
-    mole.speed = 15;
-
     // draw initial bottom line
     for (int i = 0; i < NUM_SEEDS; ++i)
     {
@@ -126,13 +91,21 @@ int main()
     }
 
     /***************************************************************************
+     * Create character
+     ****************************************************************************/
+    Sprite mole;
+    mole.counter = 0;
+    mole.image = LoadTexture("resources/mole.png");
+    mole.number_of_frames = 8;
+    mole.position.x = 30;
+    mole.position.y = 30;
+    mole.speed = 15;
+
+    /***************************************************************************
      * Main Loop
      ****************************************************************************/
     while (!WindowShouldClose()) // Detect window close button or ESC key
     {
-        scroll_world(world);
-        update_sprite(&mole);
-
         // controles
         Vector2 movement = {0, 0};
 
@@ -145,7 +118,10 @@ int main()
         if (IsKeyDown(KEY_DOWN))
             movement.y = movement.y + 1;
 
-        mole.position = Vector2Add(mole.position, movement);
+        movement = Vector2Normalize(movement);
+
+        scroll_world(world);
+        sprite_update(&mole, &movement);
 
         BeginDrawing();
         // ClearBackground(RAYWHITE);
@@ -158,7 +134,7 @@ int main()
         Vector2 origin = {0, 0};
         DrawTexturePro(screen_texture, srcRect, dstRect, origin, 0.0f, WHITE);
 
-        draw_sprite(&mole);
+        sprite_draw(&mole);
 
         EndDrawing();
     }
--- src/sprite.c
+++ src/sprite.c
@@ -8,9 +8,10 @@ typedef struct Sprite
     int counter;
     Texture2D image;
     int number_of_frames;
+    float rotation;
 } Sprite;
 
-void sprite_init(Sprite* s, const char* file, int num_of_frames, int x, int y, int speed)
+void sprite_init(Sprite *s, const char *file, int num_of_frames, int x, int y, int speed, float rotation)
 {
     s->counter = 0;
     s->image = LoadTexture(file);
@@ -18,6 +19,7 @@ void sprite_init(Sprite* s, const char* file, int num_of_frames, int x, int y, i
     s->position.x = x;
     s->position.y = y;
     s->speed = speed;
+    s->rotation = rotation;
 }
 
 void sprite_draw(Sprite *s)
@@ -27,12 +29,24 @@ void sprite_draw(Sprite *s)
     int frame_width = s->image.width / s->number_of_frames;
     int frame_height = s->image.height;
     Rectangle frame = {current_frame * frame_width, 0, frame_width, frame_height};
-    Rectangle dstRect = {s->position.x * GetScreenWidth() / WIDTH, s->position.y * GetScreenHeight() / HEIGHT, frame_width * GetScreenWidth() / WIDTH, frame_height * GetScreenHeight() / HEIGHT};
-    Vector2 origin = {0, 0};
-    DrawTexturePro(s->image, frame, dstRect, origin, 0.0f, WHITE);
+    Rectangle dstRect = {
+        s->position.x * GetScreenWidth() / WIDTH,   // x
+        s->position.y * GetScreenHeight() / HEIGHT, // y
+        frame_width * GetScreenWidth() / WIDTH,     // width
+        frame_height * GetScreenHeight() / HEIGHT   // height
+    };
+    Vector2 origin = {frame_width / 2, frame_height / 2};
+    DrawTexturePro(s->image, frame, dstRect, origin, s->rotation, WHITE);
 }
 
-void sprite_update(Sprite *s)
+void sprite_update(Sprite *s, Vector2 *movement)
 {
     s->counter = (s->counter + 1) % (s->number_of_frames * s->speed);
+    if (movement->x != 0 || movement->y != 0)
+    {
+        Vector2 one = {0, -1};
+        s->rotation = Vector2Angle(one, *move) * RAD2DEG;
+        printf("\n %f \n (%f %f)", s->rotation, movement->x, movement->y);
+        s->position = Vector2Add(s->position, *movement);
+    }
 }
