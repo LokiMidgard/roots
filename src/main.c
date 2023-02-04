b8471960f54e4637a817fcf3596b2c0b27c4551d
Tobias Krumholz <krumholt@gmail.com>
2023-02-04 02:59:09 +0100

    started some world rendering

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <conio.h>
+#include <string.h>
 #include "raylib.h"
 #include "console.c"
 
 #if defined(PLATFORM_WEB)
-    #include <emscripten/emscripten.h>
+#include <emscripten/emscripten.h>
 #endif
 
 //----------------------------------------------------------------------------------
@@ -14,42 +16,72 @@
 #define screenHeight 540
 Image screen = {0};
 
-//----------------------------------------------------------------------------------
-// Main entry point
-//----------------------------------------------------------------------------------
+int number_of_seeds = 0;
+int root_seeds[screenWidth];
+Color *world = 0;
+
+void update_last_world_line()
+{
+    memmove((char *)world, (char *)(world + screenWidth), (screenWidth * (screenHeight-1))*sizeof(Color));
+    for (int x = 1; x < screenWidth - 1; ++x)
+    {
+        int y = screenHeight - 1;
+        Color current = world[y * screenWidth + x];
+        Color next    = world[y * screenWidth + x + 1];
+        Color last    = world[y * screenWidth + x - 1];
+        if (current.r == GRAY.r)
+        {
+            if (rand()%15 < 8)
+                world[y * screenWidth + x] = GRAY;
+            else
+                world[y * screenWidth + x] = BROWN;
+        }
+        if (next.r == GRAY.r)
+        {
+            if (rand()%15 < 8)
+                world[y * screenWidth + x] = GRAY;
+        }
+    }
+}
+
 int main() 
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
     InitConsole();
     InitWindow(screenWidth, screenHeight, "raylib");
-    //--------------------------------------------------------------------------------------
 
 #if defined(PLATFORM_WEB)
     emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
 #else
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-    //
-    screen = GenImageColor(screenWidth, screenHeight, DARKBLUE);
+    SetTargetFPS(60);
+
+    screen = GenImageColor(screenWidth, screenHeight, BROWN);
     Texture2D screen_texture = LoadTextureFromImage(screen);
 
-    Color *image = LoadImageColors(screen);
-    for (int y = 0; y < 540; ++y)
-    for (int x = 0; x < 960; ++x)
+
+    world = LoadImageColors(screen);
+    number_of_seeds = 30;
+    for (int index = 0; index < number_of_seeds; ++index)
     {
-        Color color = BLACK;
-        if ((y-1) % 10 == 0 || y % 10 == 0 || (y+1) % 10 == 0)
-            color = BLUE;
-        image[x + y * 960] = color;
+        int x = rand() % screenWidth;
+        int y = screenHeight - 1;
+        for (int offset = -3; offset < 3; ++offset)
+        {
+            world[x+offset + y * screenWidth] = GRAY;
+        }
+    }
+
+    for (int index = 0; index < 10; ++index)
+    {
+        update_last_world_line();
     }
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        update_last_world_line();
         BeginDrawing();
         //ClearBackground(RAYWHITE);
-        UpdateTexture(screen_texture, image);
+        UpdateTexture(screen_texture, world);
         DrawTexture(screen_texture, 0, 0, WHITE);
         EndDrawing();
     }
@@ -58,7 +90,7 @@ int main()
     // De-Initialization
     //--------------------------------------------------------------------------------------
     CloseWindow();                  // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+                                    //--------------------------------------------------------------------------------------
     FreeConsole();
 
     return 0;
