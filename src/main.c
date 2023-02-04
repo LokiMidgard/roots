48374825af118ed1a00c7e96784f9d4dd6d68348
lqdchrm <lqdchrm-code@yahoo.de>
2023-02-04 15:01:43 +0100

    added sound

--- src/main.c
+++ src/main.c
@@ -61,6 +61,7 @@ int main()
     InitConsole();
     SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "raylib");
+    InitAudioDevice();
     SetTargetFPS(FPS);
 
     /***************************************************************************
