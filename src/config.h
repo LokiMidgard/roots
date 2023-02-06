fa4578895be1fbfb2612338a491dfb85c0cd0096
lqdchrm <lqdchrm-code@yahoo.de>
2023-02-06 03:43:16 +0100

    colors

--- src/config.h
+++ src/config.h
@@ -11,17 +11,17 @@
 #define WINDOW_HEIGHT ((int)(HEIGHT*SCALE))
 #define POS(x, y) ((y) * (WIDTH) + (x))
 
-#define TERRA_EARTH (BROWN)
-#define TERRA_TUNEL (DARKBROWN)
-#define TERRA_STONE (GRAY)
-#define TERRA_ROOT (CLITERAL(Color){ 0, 0, 0, 255 })
-#define TERRA_ROOT_TIP (CLITERAL(Color){ 1, 0, 0, 255 })
-#define TERRA_ROOT_KNOT (CLITERAL(Color){ 2, 0, 0, 255 })
-#define TERRA_EMERALD (CLITERAL(Color){ 3, 228, 48, 255 })
-#define TERRA_QUICK_STONE (CLITERAL(Color){ 3, 121, 241, 255 })
-#define TERRA_DIG_STONE (RED)
-#define TERRA_SAND (CLITERAL(Color) {237, 214, 92, 255})
-#define TERRA_LAVA (CLITERAL(Color) {255, 0, 0, 255})
+#define TERRA_EARTH         CLITERAL(Color){ 127, 106,  79, 255 }   // RAYLIB BROWN
+#define TERRA_TUNEL         CLITERAL(Color){  76,  63,  47, 255 }   // RAYLIB DARKBROWN
+#define TERRA_STONE         CLITERAL(Color){ 130, 130, 130, 255 }   // RAYLIB GRAY
+#define TERRA_ROOT          CLITERAL(Color){   0,   0,   0, 255 }   // BLACK
+#define TERRA_ROOT_TIP      CLITERAL(Color){   1,   0,   0, 255 }   // 1-RED
+#define TERRA_ROOT_KNOT     CLITERAL(Color){   2,   0,   0, 255 }   // 2-RED
+#define TERRA_EMERALD       CLITERAL(Color){   3, 228,  48, 255 }   // 3-RED
+#define TERRA_QUICK_STONE   CLITERAL(Color){   3, 121, 241, 255 }   // ???
+#define TERRA_DIG_STONE     CLITERAL(Color){ 230,  41,  55, 255 }   // ???
+#define TERRA_SAND          CLITERAL(Color){ 237, 214,  92, 255 }   //
+#define TERRA_LAVA          CLITERAL(Color){ 255,   0,   0, 255 }   // RED
 
 #define NUM_SEEDS (30)
 
